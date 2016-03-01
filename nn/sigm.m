a97d7dce8738cc8c8d7d0ffaadaf0c75462df2fb
simochen <chenx324@yeah.net>
2016-03-02 00:04:58 +0800

    add feedforward propagation

--- nn/sigm.m
+++ nn/sigm.m
@@ -1,6 +1,6 @@
 function y = sigm(x)
-%SIGMOID Compute sigmoid function
-% y = SIGMOID(x) computes the sigmoid of x.
+%SIGM Compute sigmoid function
+% y = SIGM(x) computes the sigmoid of x.
 
 y = 1.0 ./ (1.0 + exp(-x));
 end
--- nn/softmax.m
+++ nn/softmax.m
@@ -1,6 +1,7 @@
 function y = softmax(x)
-% Softmax function
-    
+%SOFTMAX Compute softmax function
+% y = SOFTMAX(x) computes the softmax of matrix x.
+    n = size(x,2);
     ex = exp(x);
-    y = ex / sum(ex);
+    y = ex ./ (sum(ex,2)*ones(1,n));
 end
